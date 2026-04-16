8662aacd0a66c9b588f2f6c7175e43bb03146681
Alexandra <smir0002@hz.nl>
2026-04-16 14:35:43 +0200

    updated database schema and seed data

--- backend/src/database/seed.sql
+++ backend/src/database/seed.sql
@@ -7,16 +7,36 @@ INSERT INTO users (name, card_uid, role, password) VALUES
 
 -- insert drawers
 INSERT INTO drawers (label, is_locked, current_weight) VALUES
-('Screws Drawer', TRUE, 0);
+('Screws Drawer', TRUE, 100.00),
+('Bolts Drawer', TRUE, 200.00);
 
 -- insert materials
 INSERT INTO materials (name, unit_weight) VALUES
-('Screw A', 0.5),
-('Screw B', 1.0),
-('Screw C', 0.5);
+('Screw A', 0.50),
+('Screw B', 1.00),
+('Screw C', 0.50),
+('Bolt A', 2.00),
+('Bolt B', 3.50);
 
 -- insert relation drawers - materials
 INSERT INTO drawer_materials (drawer_id, material_id, quantity) VALUES
+-- screws drawer
 (1, 1, 100),
 (1, 2, 50),
-(1, 3, 0);
+(1, 3, 0),
+
+-- bolt drawer
+(2, 4, 60),
+(2, 5, 20);
+
+-- Alexandra interacts wth screws drawer
+INSERT INTO logs (user_id, user_role, drawer_id, action, weight_before, weight_after) VALUES
+(1, 'student', 1, 'unlock', NULL, NULL),
+(1, 'student', 1, 'weight_update', 100.00, 95.00), -- removed 5 weight
+(1, 'student', 1, 'lock', NULL, NULL);
+
+-- Leo interacts with screws drawer
+INSERT INTO logs (user_id, user_role, drawer_id, action, weight_before, weight_after) VALUES
+(2, 'teacher', 1, 'unlock', NULL, NULL),
+(2, 'teacher', 1, 'weight_update', 95.00, 120.00), -- refill
+(2, 'teacher', 1, 'lock', NULL, NULL);
